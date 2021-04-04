e1d794d861a55ae46ff3374c22def6f5152cd180
A00447210 <fabian.vaniyamveetil.reginold@smu.ca>
2021-04-04 14:41:20 -0300

    upload

---
+++ Models/_5510Context.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace InClassDemo.Models
+{
+    public partial class _5510Context : DbContext
+    {
+        public _5510Context()
+        {
+        }
+
+        public _5510Context(DbContextOptions<_5510Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:inclassdemo.database.windows.net,1433;Initial Catalog=5510;Persist Security Info=False;User ID=fabianreginold;Password=Welcome@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                entity.Property(e => e.Addrses1).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.Emailaddress).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+
+                entity.Property(e => e.Postal).HasMaxLength(50);
+
+                entity.Property(e => e.Province).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserName)
+                    .HasName("PK__UserInfo__C9F28457FE549EC6");
+
+                entity.Property(e => e.UserName).HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Role).HasMaxLength(5);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
