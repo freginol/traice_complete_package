0b4f70aeb891bfe74414f8a7a565441e5dc07e65
A00447210 <fabian.vaniyamveetil.reginold@smu.ca>
2021-04-06 08:32:02 -0300

    pragati modify html files

---
+++ Desktop/benro/Models/OrderList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PizzaKnight.Models
+{
+    public partial class OrderList
+    {
+        public OrderList()
+        {
+            OrderItem = new HashSet<OrderItem>();
+            OrderStatus = new HashSet<OrderStatus>();
+            PaymentInfo = new HashSet<PaymentInfo>();
+        }
+
+        public string Ordid { get; set; }
+        public string Custname { get; set; }
+        public string Address { get; set; }
+        public string Ordertype { get; set; }
+        public string Discountcode { get; set; }
+        public decimal Totalamount { get; set; }
+        public int? Custid { get; set; }
+
+        public virtual Customers Cust { get; set; }
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+        public virtual ICollection<OrderStatus> OrderStatus { get; set; }
+        public virtual ICollection<PaymentInfo> PaymentInfo { get; set; }
+    }
+}
